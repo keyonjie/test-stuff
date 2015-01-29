e3dbd6c52a99e3afc5030defe626cc725805c9ea
Jie Yang <yang.jie@intel.com>
2015-01-29 14:39:11 +0800

    optimize hw to use name broadwellrt286 in test scripts

--- scripts/stress_test/loop_record.sh
+++ scripts/stress_test/loop_record.sh
@@ -3,7 +3,7 @@
 date > loop_c.log;
 #while true; do 
 for i in $(seq 1 5000); do
-	cmd="arecord -D hw:0,0 -f dat /tmp/foo.wav -d 2"; 
+	cmd="arecord -D hw:broadwellrt286,0 -f dat /tmp/foo.wav -d 2"; 
 	echo $i >> loop_c.log;
 	echo $cmd;
 	$cmd;
--- scripts/stress_test/test.sh
+++ scripts/stress_test/test.sh
@@ -1,12 +1,12 @@
 for i in $(seq 1 20000); do
   echo $i
-  arecord -D hw:1,4 -f dat -d 1 test.wav > /dev/null
+  arecord -D hw:broadwellrt286,0 -f dat -d 1 test.wav > /dev/null
   if [ $? -ne 0 ]; then
     echo $? "record failed!"
     #dmesg
     #break
   fi
-  aplay -D hw:1,0 -f dat -d 1 /dev/zero
+  aplay -D hw:broadwellrt286,0 -f dat -d 1 /dev/zero
   if [ $? -ne 0 ]; then
     echo $? "play failed!"
     #dmesg
